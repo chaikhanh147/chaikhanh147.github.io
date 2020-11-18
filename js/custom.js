d78b4b0810d8113efd9abc3ace0de4ca6d358c39
TungND <tungnd@topica.edu.vn>
2020-11-18 23:46:32 +0700

    [Update] question 1

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,3 @@
-// $(document).ready(function () {
 $(function(){
     $('.draggable').draggable({
         containment: '.box_question',
@@ -22,9 +21,6 @@ $(function(){
         },
         start: function(event, ui) {
             $(ui.helper).addClass("ui-draggable-helper");
-            // var self = $(this);
-            // let id = self.attr('id');
-            // self.data( 'number', id );
             // console.log(ui.helper.context);
             // console.log(ui.helper.clone());
         },
@@ -40,20 +36,69 @@ $(function(){
             // $(this).append(ui.draggable.clone(true));
             // $(this).droppable( "option", "disabled", true )
 
-            // var self = $(this);
-            // self.find(".list_items1").remove();
-            // // var dropId = ui.draggable.attr('id');
-            // var target_ui_object_html = ui.item.context.id;
-            // self.appendChild(target_ui_object_html);
-
-            // var itemid = $(event.originalEvent.toElement).attr("itemid");
-            // $('.box-item').each(function(index, item) {
-            //     console.log($(this).attr("itemid"), itemid);
-            //     if ($(this).attr("itemid") === itemid) {
-            //         $(this).appendTo("#" + itemid);
-            //     }
-            // });
-
+            var dropId = ui.draggable.attr('id');
+            $(this).attr('selected', dropId);
         }
+    });
+
+});
+
+$(document).ready(function () {
+    $(".btn_submit_quiz").click(function(e){
+        e.preventDefault();
+        let winners_array = [];
+        $('.droparea').each((index, item) => {
+            winners_array.push(
+                {
+                    answer : $(item).attr('answer'),
+                    selected : $(item).attr('selected')
+                }
+            )
+        });
+
+        let status = true;
+        if(winners_array.length > 0){
+            for (var key in winners_array) {
+                if(winners_array[key].answer != '' && winners_array[key].selected != ''){
+                    console.log(winners_array[key].answer, '---', winners_array[key].selected);
+                    if(winners_array[key].answer == winners_array[key].selected){
+                        status = true;
+                    }else{
+                        status = false;
+                    }
+                }else {
+                    status = false;
+                }
+                
+            }
+        }
+
+        console.log('status', status);
+        
+        if(status == true){
+            // Correct
+            $('.modal-title').text('Correct');
+            $('.modal-header').addClass('correct');
+            $('.notify').text("Đúng rồi !");
+            $('#audio').attr('src', '/audio/correct/01.m4a');
+            $('.btn_reset_hide_continue').show();
+            $('.btn_modal').click();
+        }else {
+            // Incorrect
+            $('.modal-title').text('Incorrect');
+            $('.modal-header').addClass('incorrect');
+            $('.notify').text("Chưa chính xác");
+            $('.btn_reset_hide').show();
+            $('#audio').attr('src', '/audio/incorrect/01.m4a');
+            $('.btn_modal').click();
+        }
+    });
+
+    $(".btn_reset").click(function(e){
+        location.reload();
+    });
+
+    $('.btn_reset_hide_continue').click(function(e){
+        window.location = "/weeks/week01/matching/lesson02.html";
     });
 });
